ac1b1c104a56bba093ce2cf96851754d1586feed
Blayne Chard <blayne@chard.com>
2025-03-04 13:14:36 +1300

    feat: add script to merge requests

---
+++ data/merge.sh
@@ -0,0 +1,6 @@
+
+# Read all the requests and merge them into single csv files
+for size in '32' '64' '128' '256' '512' '1024' '2048'; 
+do
+    cat req_*.json | jq ".reads[\"${size}\"]" | jq -s "flatten(1) | @csv" > read_${size}.csv
+done
